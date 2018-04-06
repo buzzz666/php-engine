1cf49ba54fbff2d5df0668158cb584d93b500d5e
buzzz666 <buzzz666666@gmail.com>
2018-04-06 17:26:45 +0300

    Update db_params.php

--- config/db_params.php
+++ config/db_params.php
@@ -1,10 +1,10 @@
 <?php 
 
 return array(
-	'host' => 'gvozdyak.mysql.ukraine.com.ua',
-	'dbname' => 'gvozdyak_roflan',
-	'user' => 'gvozdyak_roflan',
-	'password' => 'z566lkh8',
+	'host' => '@',
+	'dbname' => '@',
+	'user' => '@',
+	'password' => '@',
 );
 
 ?>
